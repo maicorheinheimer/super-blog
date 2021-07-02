96bc16f4b02cccabbfadd692a3716fb1646dd8b2
Maico Rheinheimer <maico.rheinheimer@outlook.com>
2021-07-01 20:37:32 -0400

    super blog

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="w-1/2 mx-auto my-20">
+    <h1 class="text-3xl font-semibold text-red-600">
+      {{tituloBlog}}
+    </h1>
+    <article v-for="(artigo, index) in listaArtigos" :key="index" class="my-20">
+      <h2 class="text-5xl font-semibold mb-5">{{artigo.titulo}}</h2>
+      <p class="mb-5">{{artigo.resumo}}</p>
+      <p class="text-red-500">Ler Mais</p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      listaArtigos: [],
+      tituloBlog: "Super Blog",
+    };
+  },
+  created(){
+    this.carregarListaArtigos();  
+  },
+  methods: {
+    carregarListaArtigos() {
+      this.$axios
+        .get("https://api")
+        .then((respostaAPI) => {
+          this.listaArtigos = respostaAPI.data;   
+        });
+    },
+  },
+};
+</script>
